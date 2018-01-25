656c630bc8a75c49e3ed153ec99e7b9ac49e6b03
jimmybae <mywindbell@gmail.com>
2018-01-25 16:51:31 +0900

    change file read-write to copy

--- actions/init-static.js
+++ actions/init-static.js
@@ -1,7 +1,6 @@
 const fs = require('fs');
 const logger = require('../logger.js');
-const CURR_DIR = process.cwd();
-
+const projectNameSubfix = '';
 const createDirectoryContents = (templatePath, newProjectPath) => {
   const filesToCreate = fs.readdirSync(templatePath);
   filesToCreate.forEach(file => {
@@ -9,13 +8,20 @@ const createDirectoryContents = (templatePath, newProjectPath) => {
     // get stats about the current file
     const stats = fs.statSync(origFilePath);
     if (stats.isFile()) {
-      const contents = fs.readFileSync(origFilePath, 'utf8');
-      
       const writePath = `${newProjectPath}/${file}`;
-      fs.writeFileSync(writePath, contents, 'utf8');
+      fs.copyFileSync(origFilePath, writePath);
+      /* jimmy 그냥 copy하면 되는데 read, write를 한다는건
+       * 아마도 파일 내용을 변경하기 위해서 그런듯
+       * 추후에 좀더 파악 필요함.
+      if(file.indexOf('png') > -1) {
+        fs.copyFileSync(origFilePath, writePath);
+      } else {
+        const contents = fs.readFileSync(origFilePath, 'utf8');
+        fs.writeFileSync(writePath, contents, 'utf8');
+      }
+      */
     } else if (stats.isDirectory()) {
       fs.mkdirSync(`${newProjectPath}/${file}`);
-      
       // recursive call
       createDirectoryContents(`${templatePath}/${file}`, `${newProjectPath}/${file}`);
     }
@@ -24,12 +30,12 @@ const createDirectoryContents = (templatePath, newProjectPath) => {
 
 module.exports = (projectName, projectType) => {
   const templatePath = `templates-static/${projectType}`;
-  const newProjectPath = `${projectName}-static`;
+  const newProjectPath = `${projectName}${projectNameSubfix}`;
   if (!fs.existsSync(newProjectPath)) {
     fs.mkdirSync(newProjectPath);
     createDirectoryContents(templatePath, newProjectPath);
-    logger.log(`Project <${projectName}-static> initialized successfuly.`);
+    logger.log(`Project <${projectName}${projectNameSubfix}> initialized successfuly.`);
   } else {
-    logger.error(`Project <${projectName}-static> already exist.`);
+    logger.error(`Project <${projectName}${projectNameSubfix}> already exist.`);
   }
 };
